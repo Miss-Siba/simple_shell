15540ec126fb6c7b7dcbc7e8b83ec6421b8b8e5c
TshilidziT <Tshilidzibytee@gmail.com>
2023-11-05 08:22:24 -0800

    Over to you

---
+++ multiple.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_LINE 80
+
+int main(void)
+{
+	char *line;
+	size_t len = MAX_LINE;
+	char *args[MAX_LINE / 2 + 1];
+	int should_run = 1;
+	int i;
+	ssize_t nread;
+
+	line = malloc(len);
+	if (line == NULL)
+	{
+	perror("malloc");
+	exit(1);
+	}
+
+	while (should_run)
+	{
+		printf("dreamteam$ ");
+		fflush(stdout);
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+		{
+			perror("getline");
+			break;
+		}
+		line[nread - 1] = '\0';
+		if (strcmp(line, "exit") == 0)
+		{
+			should_run = 0;
+		}
+		else
+		{
+			i = 0;
+			args[i] = strtok(line, " \t");
+
+			while (args[i] != NULL)
+			{
+				i++;
+				args[i] = strtok(NULL, " \t");
+			}
+			args[i] = NULL;
+			pid_t pid = fork();
+
+			if (pid < 0)
+			{
+				perror("fork");
+				exit(1);
+			}
+			else if (pid == 0)
+			{
+				if (execvp(args[0], args) < 0)
+				{
+					perror(args[0]);
+					exit(1);
+				}
+			}
+			else
+			{
+				wait(NULL);
+			}
+		}
+	}
+	free(line);
+	return (0);
+}
+
